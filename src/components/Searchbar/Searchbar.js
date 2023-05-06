21b6095f4b0ffe091e0c5a40a117fa60a740ff46
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-05-07 00:32:33 +0300

    add hooks for Searchbar

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+// import { Component, useState } from 'react';
+import { useState } from 'react';
+
 import {
   ButtonSearch,
   InputSearch,
@@ -9,49 +11,91 @@ import {
 } from './Searchbar.styled';
 import { AiOutlineSearch } from 'react-icons/ai';
 
-export class Searchbar extends Component {
-  static propTypes = {
-    onSearch: PropTypes.func.isRequired,
+// export class Searchbar extends Component {
+//   static propTypes = {
+//     onSearch: PropTypes.func.isRequired,
+//   };
+
+//   state = {
+//     searchName: '',
+//   };
+
+//   handleSearchNameChange = e => {
+//     this.setState({ searchName: e.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     if (this.state.searchName.trim() === '') {
+//       return alert('Enter image name or photo name');
+//     }
+
+//     this.props.onSearch(this.state.searchName);
+//     this.setState({ searchName: '' });
+//   };
+
+//   render() {
+//     return (
+//       <SearchbarCont>
+//         <SearchForm onSubmit={this.handleSubmit}>
+//           <ButtonSearch type="submit">
+//             <AiOutlineSearch size={48} />
+//             <LabelBtn>Search</LabelBtn>
+//           </ButtonSearch>
+
+//           <InputSearch
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             value={this.state.searchName}
+//             onChange={this.handleSearchNameChange}
+//           />
+//         </SearchForm>
+//       </SearchbarCont>
+//     );
+//   }
+// }
+
+export const Searchbar = ({ onSearch }) => {
+  const [searchName, setSearchName] = useState('');
+
+  const handleSearchNameChange = e => {
+    setSearchName(e.currentTarget.value.toLowerCase());
   };
 
-  state = {
-    searchName: '',
-  };
-
-  handleSearchNameChange = e => {
-    this.setState({ searchName: e.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (this.state.searchName.trim() === '') {
+    if (searchName.trim() === '') {
       return alert('Enter image name or photo name');
     }
-
-    this.props.onSearch(this.state.searchName);
-    this.setState({ searchName: '' });
+    onSearch(searchName);
+    setSearchName('');
   };
 
-  render() {
-    return (
-      <SearchbarCont>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <ButtonSearch type="submit">
-            <AiOutlineSearch size={48} />
-            <LabelBtn>Search</LabelBtn>
-          </ButtonSearch>
+  return (
+    <SearchbarCont>
+      <SearchForm onSubmit={handleSubmit}>
+        <ButtonSearch type="submit">
+          <AiOutlineSearch size={48} />
+          <LabelBtn>Search</LabelBtn>
+        </ButtonSearch>
 
-          <InputSearch
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.searchName}
-            onChange={this.handleSearchNameChange}
-          />
-        </SearchForm>
-      </SearchbarCont>
-    );
-  }
-}
+        <InputSearch
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={searchName}
+          onChange={handleSearchNameChange}
+        />
+      </SearchForm>
+    </SearchbarCont>
+  );
+};
+
+Searchbar.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+};
