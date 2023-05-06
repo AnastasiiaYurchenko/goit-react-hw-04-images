bde631ebdbf0d96901ffe3cefda5c4284ac956c7
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-05-07 00:20:44 +0300

    add useState

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,48 +1,80 @@
 import PropTypes from 'prop-types';
 import { ImageModal } from 'components/ImageModal/ImageModal';
 import { GalleryItem, Image } from './ImageGalleryItem.styled';
-import { Component } from 'react';
+import { useState } from 'react';
 
-export class ImageGalleryItem extends Component {
-  static propTypes = {
-    picture: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      largeImageURL: PropTypes.string.isRequired,
-      tags: PropTypes.string.isRequired,
-      webformatURL: PropTypes.string.isRequired,
-    }).isRequired,
-  };
+// export class ImageGalleryItem extends Component {
+//   static propTypes = {
+//     picture: PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       largeImageURL: PropTypes.string.isRequired,
+//       tags: PropTypes.string.isRequired,
+//       webformatURL: PropTypes.string.isRequired,
+//     }).isRequired,
+//   };
 
-  state = {
-    selectedImage: null,
-  };
+//   state = {
+//     selectedImage: null,
+//   };
 
-  setSelectedImage = () => {
-    this.setState({ selectedImage: this.props.picture.largeImageURL });
-  };
+//   setSelectedImage = () => {
+//     this.setState({ selectedImage: this.props.picture.largeImageURL });
+//   };
 
-  closeModal = () => {
-    this.setState({ selectedImage: null });
-  };
+//   closeModal = () => {
+//     this.setState({ selectedImage: null });
+//   };
 
-  render() {
-    const { selectedImage } = this.state;
-    const { picture } = this.props;
-    return (
-      <GalleryItem>
-        <Image
-          src={picture.webformatURL}
-          alt={picture.tags}
-          onClick={this.setSelectedImage}
+//   render() {
+//     const { selectedImage } = this.state;
+//     const { picture } = this.props;
+//     return (
+//       <GalleryItem>
+//         <Image
+//           src={picture.webformatURL}
+//           alt={picture.tags}
+//           onClick={this.setSelectedImage}
+//         />
+//         {selectedImage && (
+//           <ImageModal
+//             image={selectedImage}
+//             isOpen={selectedImage !== null}
+//             onClose={this.closeModal}
+//           />
+//         )}
+//       </GalleryItem>
+//     );
+//   }
+// }
+
+export const ImageGalleryItem = ({
+  picture: { webformatURL, tags, largeImageURL },
+}) => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  return (
+    <GalleryItem>
+      <Image
+        src={webformatURL}
+        alt={tags}
+        onClick={() => setSelectedImage(largeImageURL)}
+      />
+      {selectedImage && (
+        <ImageModal
+          image={selectedImage}
+          isOpen={selectedImage !== null}
+          onClose={() => setSelectedImage(null)}
         />
-        {selectedImage && (
-          <ImageModal
-            image={selectedImage}
-            isOpen={selectedImage !== null}
-            onClose={this.closeModal}
-          />
-        )}
-      </GalleryItem>
-    );
-  }
-}
+      )}
+    </GalleryItem>
+  );
+};
+
+ImageGalleryItem.propTypes = {
+  picture: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+    webformatURL: PropTypes.string.isRequired,
+  }).isRequired,
+};
