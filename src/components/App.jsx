98f146432713ccd784a60b6413ce48e5073830dd
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-05-08 23:07:31 +0300

    delete comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,76 +3,10 @@ import { Layout } from './Layout/Layout.styled';
 import { Searchbar } from './Searchbar/Searchbar';
 import { Button } from './Button/Button';
 import { useState, useEffect } from 'react';
-// import { Component } from 'react';
-
 import { Loader } from './Loader/Loader';
 import * as API from '../api';
 
 export const ERROR_MSG = 'Something went wrong, please try again';
-
-// export class App extends Component {
-//   state = {
-//     searchName: '',
-//     pictures: [],
-//     loading: false,
-//     error: null,
-//     page: 1,
-//   };
-
-//   handleFormSearch = searchName => {
-//     this.setState({ searchName, page: 1, pictures: [] });
-//   };
-
-//   async componentDidUpdate(prevProps, prevState) {
-//     if (
-//       prevState.searchName !== this.state.searchName ||
-//       prevState.page !== this.state.page
-//     ) {
-//       this.setState({ loading: true, error: null });
-
-//       try {
-//         const fetchedPictures = await API.fetchPictures(
-//           this.state.searchName,
-//           this.state.page
-//         );
-
-//         this.setState(prevState => {
-//           return {
-//             pictures: [...prevState.pictures, ...fetchedPictures.hits],
-//           };
-//         });
-//       } catch (error) {
-//         this.setState({ error: ERROR_MSG });
-//       } finally {
-//         this.setState({ loading: false });
-//       }
-//     }
-//   }
-
-//   handleLoadMore = e => {
-//     const { page } = this.state;
-//     this.setState({ page: page + 1 });
-//   };
-
-//   render() {
-//     const { loading, error, pictures, searchName } = this.state;
-//     return (
-//       <Layout>
-//         <Searchbar onSearch={this.handleFormSearch} />
-
-//         {error && <h1>{this.state.error} </h1>}
-//         {pictures && (
-//           <ImageGallery searchName={searchName} pictures={pictures} />
-//         )}
-//         {loading && <Loader />}
-
-//         {pictures && pictures.length > 0 && (
-//           <Button onClick={this.handleLoadMore} />
-//         )}
-//       </Layout>
-//     );
-//   }
-// }
 
 export const App = () => {
   const [searchName, setSearchName] = useState('');
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
-// import { Component, useState } from 'react';
 import { useState } from 'react';
-
 import {
   ButtonSearch,
   InputSearch,
@@ -10,53 +8,6 @@ import {
   SearchbarCont,
 } from './Searchbar.styled';
 import { AiOutlineSearch } from 'react-icons/ai';
-
-// export class Searchbar extends Component {
-//   static propTypes = {
-//     onSearch: PropTypes.func.isRequired,
-//   };
-
-//   state = {
-//     searchName: '',
-//   };
-
-//   handleSearchNameChange = e => {
-//     this.setState({ searchName: e.currentTarget.value.toLowerCase() });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-
-//     if (this.state.searchName.trim() === '') {
-//       return alert('Enter image name or photo name');
-//     }
-
-//     this.props.onSearch(this.state.searchName);
-//     this.setState({ searchName: '' });
-//   };
-
-//   render() {
-//     return (
-//       <SearchbarCont>
-//         <SearchForm onSubmit={this.handleSubmit}>
-//           <ButtonSearch type="submit">
-//             <AiOutlineSearch size={48} />
-//             <LabelBtn>Search</LabelBtn>
-//           </ButtonSearch>
-
-//           <InputSearch
-//             type="text"
-//             autoComplete="off"
-//             autoFocus
-//             placeholder="Search images and photos"
-//             value={this.state.searchName}
-//             onChange={this.handleSearchNameChange}
-//           />
-//         </SearchForm>
-//       </SearchbarCont>
-//     );
-//   }
-// }
 
 export const Searchbar = ({ onSearch }) => {
   const [searchName, setSearchName] = useState('');
